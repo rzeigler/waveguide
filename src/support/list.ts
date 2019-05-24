e6c5388d2983aa97ea2f6f76a21e461d884287ba
Ryan Zeigler <zeiglerr@gmail.com>
2019-05-24 15:10:14 -0500

    Begin re-implementing list

--- src/support/list.ts
+++ src/support/list.ts
@@ -16,168 +16,192 @@ import { array } from "fp-ts/lib/Array";
 import { FunctionN, Lazy, Predicate } from "fp-ts/lib/function";
 import { none, Option, some } from "fp-ts/lib/Option";
 
-export type List<A> = Cons<A> | Nil<A>;
+export type List<A> = Cons<A> | Nil;
 
-export class Cons<A> {
-  public readonly _tag: "cons" = "cons";
-  constructor(public readonly a: A, public readonly rest: List<A>) { }
-
-  public prepend(a: A): List<A> {
-    return new Cons(a, this);
-  }
-
-  public cata<B>(ifNil: B, ifCons: FunctionN<[A, List<A>], B>): B {
-    return ifCons(this.a, this.rest);
-  }
-
-  public cataL<B>(ifNil: Lazy<B>, ifCons: FunctionN<[A, List<A>], B>): B {
-    return ifCons(this.a, this.rest);
-  }
-
-  public head(): Option<A> {
-    return some(this.a);
-  }
-
-  public tail(): Option<List<A>> {
-    return some(this.rest);
-  }
-
-  public foldl<B>(b: B, f: FunctionN<[B, A], B>): B {
-    let current: List<A> = this;
-    while (current._tag === "cons") {
-      b = f(b, current.a);
-      current = current.rest;
-    }
-    return b;
-  }
-
-  public reverse(): List<A> {
-    return this.foldl<List<A>>(nil, (tail, head) => tail.prepend(head));
-  }
-
-  public drop(n: number): List<A> {
-    let current: List<A> = this;
-    while (n > 0 && current._tag === "cons") {
-      current = this.rest;
-    }
-    return current;
-  }
-
-  public init(): Option<List<A>> {
-    return some(this.reverse()
-      .drop(1)
-      .reverse());
-  }
-
-  public last(): Option<A> {
-    let head: A = this.a;
-    let tail: List<A> = this.rest;
-    while (tail._tag !== "nil") {
-      head = tail.a;
-      tail = tail.rest;
-    }
-    return some(head);
-  }
-
-  public filter(f: Predicate<A>): List<A> {
-    return this.foldl<List<A>>(nil, (tail, head) => f(head) ? tail.prepend(head) : tail);
-  }
-
-  public find(f: Predicate<A>): Option<A> {
-    let current: List<A> = this;
-    while (current._tag === "cons") {
-      if (f(current.a)) {
-        return some(current.a);
-      }
-      current = current.rest;
-    }
-    return none;
-  }
-
-  public isEmpty(): this is Nil<A> {
-    return false;
-  }
-
-  public size(): number {
-    let current: List<A> = this;
-    let size = 0;
-    while (current._tag !== "nil") {
-      size++;
-      current = current.rest;
-    }
-    return size;
-  }
+export interface Cons<A> {
+  readonly _tag: "cons";
+  readonly head: A;
+  readonly tail: List<A>;
 }
 
-export class Nil<A> {
-  public readonly _tag: "nil" = "nil";
-
-  public prepend(a: A): List<A> {
-    return new Cons(a, this);
-  }
-
-  public cata<B>(ifNil: B, ifCons: FunctionN<[A, List<A>], B>): B {
-    return ifNil;
-  }
-
-  public cataL<B>(ifNil: Lazy<B>, ifCons: FunctionN<[A, List<A>], B>): B {
-    return ifNil();
-  }
-
-  public head(): Option<A> {
-    return none;
-  }
-
-  public tail(): Option<List<A>> {
-    return none;
-  }
-
-  public foldl<B>(b: B, f: FunctionN<[B, A], B>): B {
-    return b;
-  }
-
-  public reverse(): List<A> {
-    return this;
-  }
-
-  public filter(): List<A> {
-    return this;
-  }
-
-  public drop(n: number): List<A> {
-    return this;
-  }
-
-  public init(): Option<List<A>> {
-    return none;
-  }
-
-  public last(): Option<A> {
-    return none;
-  }
-
-  public isEmpty(): this is Nil<A> {
-    return true;
-  }
-
-  public find(f: Predicate<A>): Option<A> {
-    return none;
-  }
-
-  public size(): number {
-    return 0;
-  }
+export interface Nil {
+  readonly _tag: "nil";
 }
 
-const nil: Nil<never> = new Nil();
+export const nil: List<never> = { _tag: "nil" };
 
-function prepend<A>(a: A, rest: List<A>): List<A> {
-  return rest.prepend(a);
+export function cons<A>(head: A, tail: List<A>): List<A> {
+  return {
+    _tag: "cons",
+    head,
+    tail
+  };
 }
 
-function fromArray<A>(as: A[]): List<A> {
-  return array.reduce(as, nil as List<A>, (l, a) => new Cons(a, l));
+export function prepend$<A>(tail: List<A>, a: A): List<A> {
+  return cons(a, tail);
 }
+
+// export class Cons<A> {
+//   public readonly _tag: "cons" = "cons";
+//   constructor(public readonly a: A, public readonly rest: List<A>) { }
+
+//   public prepend(a: A): List<A> {
+//     return new Cons(a, this);
+//   }
+
+//   public cata<B>(ifNil: B, ifCons: FunctionN<[A, List<A>], B>): B {
+//     return ifCons(this.a, this.rest);
+//   }
+
+//   public cataL<B>(ifNil: Lazy<B>, ifCons: FunctionN<[A, List<A>], B>): B {
+//     return ifCons(this.a, this.rest);
+//   }
+
+//   public head(): Option<A> {
+//     return some(this.a);
+//   }
+
+//   public tail(): Option<List<A>> {
+//     return some(this.rest);
+//   }
+
+//   public foldl<B>(b: B, f: FunctionN<[B, A], B>): B {
+//     let current: List<A> = this;
+//     while (current._tag === "cons") {
+//       b = f(b, current.a);
+//       current = current.rest;
+//     }
+//     return b;
+//   }
+
+//   public reverse(): List<A> {
+//     return this.foldl<List<A>>(nil, (tail, head) => tail.prepend(head));
+//   }
+
+//   public drop(n: number): List<A> {
+//     let current: List<A> = this;
+//     while (n > 0 && current._tag === "cons") {
+//       current = this.rest;
+//     }
+//     return current;
+//   }
+
+//   public init(): Option<List<A>> {
+//     return some(this.reverse()
+//       .drop(1)
+//       .reverse());
+//   }
+
+//   public last(): Option<A> {
+//     let head: A = this.a;
+//     let tail: List<A> = this.rest;
+//     while (tail._tag !== "nil") {
+//       head = tail.a;
+//       tail = tail.rest;
+//     }
+//     return some(head);
+//   }
+
+//   public filter(f: Predicate<A>): List<A> {
+//     return this.foldl<List<A>>(nil, (tail, head) => f(head) ? tail.prepend(head) : tail);
+//   }
+
+//   public find(f: Predicate<A>): Option<A> {
+//     let current: List<A> = this;
+//     while (current._tag === "cons") {
+//       if (f(current.a)) {
+//         return some(current.a);
+//       }
+//       current = current.rest;
+//     }
+//     return none;
+//   }
+
+//   public isEmpty(): this is Nil<A> {
+//     return false;
+//   }
+
+//   public size(): number {
+//     let current: List<A> = this;
+//     let size = 0;
+//     while (current._tag !== "nil") {
+//       size++;
+//       current = current.rest;
+//     }
+//     return size;
+//   }
+// }
+
+// export class Nil<A> {
+//   public readonly _tag: "nil" = "nil";
+
+//   public prepend(a: A): List<A> {
+//     return new Cons(a, this);
+//   }
+
+//   public cata<B>(ifNil: B, ifCons: FunctionN<[A, List<A>], B>): B {
+//     return ifNil;
+//   }
+
+//   public cataL<B>(ifNil: Lazy<B>, ifCons: FunctionN<[A, List<A>], B>): B {
+//     return ifNil();
+//   }
+
+//   public head(): Option<A> {
+//     return none;
+//   }
+
+//   public tail(): Option<List<A>> {
+//     return none;
+//   }
+
+//   public foldl<B>(b: B, f: FunctionN<[B, A], B>): B {
+//     return b;
+//   }
+
+//   public reverse(): List<A> {
+//     return this;
+//   }
+
+//   public filter(): List<A> {
+//     return this;
+//   }
+
+//   public drop(n: number): List<A> {
+//     return this;
+//   }
+
+//   public init(): Option<List<A>> {
+//     return none;
+//   }
+
+//   public last(): Option<A> {
+//     return none;
+//   }
+
+//   public isEmpty(): this is Nil<A> {
+//     return true;
+//   }
+
+//   public find(f: Predicate<A>): Option<A> {
+//     return none;
+//   }
+
+//   public size(): number {
+//     return 0;
+//   }
+// }
+
+// const nil: Nil<never> = new Nil();
+
+// function prepend<A>(a: A, rest: List<A>): List<A> {
+//   return rest.prepend(a);
+// }
+
+// function fromArray<A>(as: A[]): List<A> {
+//   return array.reduce(as, nil as List<A>, (l, a) => new Cons(a, l));
+// }
 
 export const list = {
   nil,
